7840c01821f48c7b7942d3610cb379391fb92d0f
Amos <379833553@qq.com>
2017-07-08 11:59:21 +0800

    updata run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+from proxypool.web_api import app
+from proxypool.schedule import Schedule
+from proxypool.settings import WEBAPI_ENABLED
+
+def proxypool_run():
+    s = Schedule()
+    s.run()
+    if WEBAPI_ENABLED:
+        app.run()
+
+if __name__ == '__main__':
+    proxypool_run()
